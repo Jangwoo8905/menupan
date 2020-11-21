cf79e320449a5d85734b6038dadcc8772ff053d2
Youngiyong <youn9354@naver.com>
2020-11-21 20:04:24 +0900

    돌겠넹

--- target/menupan-1.0-SNAPSHOT/js/main.js
+++ target/menupan-1.0-SNAPSHOT/js/main.js
@@ -1,12 +1,31 @@
-var img=new Array();
-
-for(var i=1; i<=10; i++){
-    img[i]=new Image(); img[i].src="../images/conversion/"+i+".png";
+const all = ele => document.querySelectorAll(ele)
+const one = ele => document.querySelector(ele)
+const slide = _ => {
+    const wrap = one('.slide') // .slide 선택
+    const target = wrap.children[0] // .slide ul 선택
+    const len = target.children.length // .slide li 갯수
+    // .slide ul의 너비 조정
+    target.style.cssText = `width:calc(100% * ${len});display:flex;transition:1s`
+    // .slide li의 너비 조정
+    Array.from(target.children)
+        .forEach(ele => ele.style.cssText = `width:calc(100% / ${len});`)
+    // 화면 전환 실행
+    let pos = 0
+    setInterval(() => {
+        pos = (pos + 1) % len // 장면 선택
+        target.style.marginLeft = `${-pos * 100}%`
+    }, 1000) // 1500 = 1500ms = 1.5sec. 즉, 1.5초 마다 실행
 }
+window.onload = function () {
+    slide()
+}
+
+/*var img=new Array();
+
 
 var interval=1000;
 var n=0;
-var imgs = new Array("../images/test/1.png","../images/test/2.png","../images/test/3.png", "../images/test/4.png", "../images/test/5.png", "../images/test/6.png", "../images/test/7.png", "../images/test/8.png", "../images/test/9.png", "../images/test/10.png" );
+var imgs = new Array("../images/conversion/1.png","../images/conversion/2.png","../images/conversion/3.png", "../images/conversion/4.png", "../images/conversion/5.png", "../images/conversion/6.png", "../images/conversion/7.png", "../images/conversion/8.png", "../images/conversion/9.png", "../images/conversion/10.png" );
 function rotate()
 {
     if(navigator.appName=="Netscape" && document.getElementById)
@@ -29,5 +48,5 @@ function getdate(){
 
     console.log(result);
     div.innerHTML = result;
-}
+}*/
 
